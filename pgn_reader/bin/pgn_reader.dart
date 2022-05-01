082d8b41d29e8ba6fe1b758c585adfa61138d913
Sami El Sayed <selsayed53@gmail.com>
2022-05-01 16:42:05 -0400

    Test Push

--- pgn_reader/bin/pgn_reader.dart
+++ pgn_reader/bin/pgn_reader.dart
@@ -2,11 +2,17 @@ import 'dart:io';
 import 'package:chess/chess.dart' as ch;
 import 'package:pgn_reader/pgn_reader.dart' as pgn_reader;
 
+class GameModel {
+  GameModel({this.previousmove, this.variations = const []});
+  GameModel? previousmove;
+  List variations;
+}
+
 void main(List<String> arguments) {
   ch.Chess game = ch.Chess();
   //File caroPGN = File("lib/CaroKann.pgn");
-  String samplePGN =
-      "1. e4 e5 (1... e6 2. b3 d5 3. Bb2) 2. Nc3 Nf6 3. f4 exf4 4. e5";
-  game.load_pgn(samplePGN);
+  game.move("e4");
+  game.move("e5");
+  print(game.pgn());
   print('Hello world: ${pgn_reader.calculate()}!');
 }
